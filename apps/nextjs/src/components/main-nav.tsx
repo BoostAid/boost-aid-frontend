80292b5652477c12ea5e250179c78aec79ae197d
BestMarkStevenson <164673883+BestMarkStevenson@users.noreply.github.com>
2024-08-20 12:36:36 -0400

    Put Shiba Logo back in, changed Shibarium text to orange, made button red

--- apps/nextjs/src/components/main-nav.tsx
+++ apps/nextjs/src/components/main-nav.tsx
@@ -22,45 +22,45 @@ interface MainNavProps {
 export function MainNav({ items, children, params: { lang } }: MainNavProps) {
   const segment = useSelectedLayoutSegment();
   const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false);
+
   const toggleMenu = () => {
     setShowMobileMenu(!showMobileMenu);
   };
+
   const handleMenuItemClick = () => {
     toggleMenu();
   };
+
   return (
-    <div className="flex gap-6 md:gap-10">
-      <Link href={`/${lang}`} className="hidden items-center space-x-2 md:flex">
+    <div
+      className="flex pt-6" // Increased top padding
+      style={{
+        gap: '0.5rem',
+        marginLeft: '-1rem', // Move logos to the left
+        transform: 'translateY(-0.5rem)', // Move logos up
+      }}
+    >
+      <Link href={`/${lang}`} className="hidden items-center md:flex">
         <div>
           <Image
             src="/images/avatars/BoostAidLogo.svg"
             width="150"
             height="150"
-            alt=""
+            alt="BoostAid Logo"
           />
         </div>
       </Link>
 
-      {/* 
-      {items?.length ? (
-        <nav className="hidden gap-6 md:flex">
-          {items?.map((item, index) => (
-            <Link
-              key={index}
-              href={item.disabled ? "#" : `/${lang}${item.href}`}
-              className={cn(
-                "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm",
-                item.href.startsWith(`/${segment}`)
-                  ? "text-foreground"
-                  : "text-foreground/60",
-                item.disabled && "cursor-not-allowed opacity-80",
-              )}
-            >
-              {item.title}
-            </Link>
-          ))}
-        </nav>
-      ) : null} */}
+      <Link href={`/${lang}`} className="hidden items-center md:flex">
+        <div>
+          <Image
+            src="/images/avatars/ShibaInuLogo.svg"
+            width="45"
+            height="45"
+            alt="Shiba Inu Logo"
+          />
+        </div>
+      </Link>
 
       <button
         className="flex items-center space-x-2 md:hidden"
@@ -69,6 +69,7 @@ export function MainNav({ items, children, params: { lang } }: MainNavProps) {
         {showMobileMenu ? <Icons.Close /> : <Icons.Logo />}
         <span className="font-bold">Menu</span>
       </button>
+
       {showMobileMenu && items && (
         <MobileNav items={items} menuItemClick={handleMenuItemClick}>
           {children}
